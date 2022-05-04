a84c3c8355c50405489e82e1e7098130f0b7bc4b
mohadeev <94206815+mohadeev@users.noreply.github.com>
2022-05-05 01:48:36 +0200

    dsf

--- db/schema/Message.js
+++ db/schema/Message.js
@@ -6,17 +6,13 @@ const MessageSchema = mongoose.Schema(
       type: String,
       required: true,
     },
-    password: {
-      type: String,
-    },
+   
 
     sender: {
       type: String,
-      required: true
     },
     to: {
       type: String,
-      required: true
     },
   },
   { timestamps: true }
