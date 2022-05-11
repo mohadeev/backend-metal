b6ddb122a58581491d9940008a6054c7750cd35d
mohadeev <94206815+mohadeev@users.noreply.github.com>
2022-05-11 19:20:27 +0200

    df

--- routes/socket/sendmessage.js
+++ routes/socket/sendmessage.js
@@ -1,9 +1,11 @@
 const sendmessage = (socket, AllUsers, io) => {
   socket.on("send-messageto-user", (data) => {
     io.to(data.conversationId).emit("get-message", data);
+    io.in(data.conversationId).emit("get-message5", data);
     socket.to(data.conversationId).emit("get-message1", data);
     socket.emit("get-message2", data);
-    console.log("heyyyyy:" , AllUsers);
+    console.log("heyyyyy:", AllUsers);
+
     socket.broadcast.to(data.conversationId).emit("get-message3", data);
     socket.broadcast.emit("get-message4", data);
     // console.log(data.conversationId);
