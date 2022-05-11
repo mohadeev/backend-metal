eb0823cab47e7d9238975c7f0f3049123415d316
mohadeev <94206815+mohadeev@users.noreply.github.com>
2022-05-11 20:05:38 +0200

    r

--- routes/socket/sendmessage.js
+++ routes/socket/sendmessage.js
@@ -1,10 +1,19 @@
+import Converstion from "../../db/schema/Converstions.js";
+
 const sendmessage = (socket, AllUsers, io) => {
-  socket.on("send-messageto-user", (data) => {
+  socket.on("send-messageto-user", async (data) => {
+    const daddd = await Converstion.findOne({
+      _id: data.conversationId,
+    });
+    const users = daddd.map((itmes) => {
+      return itmes.members;
+    });
+    console.log(users);
     io.to(data.conversationId).emit("get-message", data);
     socket.to(data.conversationId).emit("get-message1", data);
     socket.emit("get-message2", data);
-    console.log("heyyyyy:", AllUsers);
-
+    // console.log("heyyyyy:", AllUsers);
+//
     socket.broadcast.to(data.conversationId).emit("get-message3", data);
     // socket.broadcast.emit("get-message4", data);
     io.in(data.conversationId).emit("get-message5", data);
