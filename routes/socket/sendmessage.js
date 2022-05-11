302fa200c051e63f30c36c818eaf070d43622351
mohadeev <94206815+mohadeev@users.noreply.github.com>
2022-05-11 19:47:27 +0200

    sdf

--- routes/socket/sendmessage.js
+++ routes/socket/sendmessage.js
@@ -6,7 +6,7 @@ const sendmessage = (socket, AllUsers, io) => {
     console.log("heyyyyy:", AllUsers);
 
     socket.broadcast.to(data.conversationId).emit("get-message3", data);
-    socket.broadcast.emit("get-message4", data);
+    // socket.broadcast.emit("get-message4", data);
     io.in(data.conversationId).emit("get-message5", data);
     // console.log(data.conversationId);
   });
