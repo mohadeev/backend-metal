5bbf3adf3143587e8a07fa6f8d2b37f93663620d
mohadeev <94206815+mohadeev@users.noreply.github.com>
2022-09-19 07:18:04 +0200

    SFSFSFSF

--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -1,10 +1,20 @@
 import express from "express";
+import AuthToken from "../../utils/verify-user/VerifyUser.js";
 import routerSignIn from "./signin/signin.js";
 import routerSignUp from "./signup/signup.js";
+import User from "../../db/schema/user.js";
 
 const routesAuth = express.Router();
 
 routesAuth.use("/auth/sign-in", routerSignIn);
 routesAuth.use("/auth/sign-up", routerSignUp);
-
+routesAuth.get("/get/user-data/:token", AuthToken, (req, res) => {
+  const useId = req.userId;
+  User.findOne({ _id: useId }).then((userData) => {
+    console.log(userData);
+    if (userData) {
+      res.json({ responseData: userData });
+    }
+  });
+});
 export default routesAuth;
