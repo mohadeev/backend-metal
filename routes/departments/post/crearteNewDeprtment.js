b1c5580874689b355e2e865c86f48ecd2b383077
mohadeev <94206815+mohadeev@users.noreply.github.com>
2023-03-01 12:59:01 +0100

    uiuiui

--- routes/departments/post/crearteNewDeprtment.js
+++ routes/departments/post/crearteNewDeprtment.js
@@ -7,26 +7,30 @@ import resFuncError from "../../../utils/resFuncError.js";
 const crearteNewDeprtment = express.Router();
 
 crearteNewDeprtment.post("/", async (req, res) => {
-  const id = "63fc0305876826ff032d68cb";
+  const id = "63fef4c2ab330b3f117acdf4";
   const depsData = req.body.deprtments;
   console.log("depsData", depsData);
-  const myObjectId = mongoose.Types.ObjectId(id);
-  try {
-    await departmentsModal.updateOne(
-      { _id: myObjectId },
-      { departments: depsData }
-    );
-    departmentsModal.find({}).then((channels) => {
-      const newObjectId = new mongoose.Types.ObjectId();
-      // console.log("channels", channels);
-      if (channels.length) {
-        // console.log(channels[0]?.departments);
-        res.json({ responseData: channels[0].departments });
-      } else {
-        res.json({ responsMessage: "NoChanelFounded" });
-      }
-    });
-  } catch (error) {}
+
+  departmentsModal.find({}).then(async (depty) => {
+    const IdDepart = depty[0]._id;
+    const myObjectId = mongoose.Types.ObjectId(IdDepart);
+    try {
+      await departmentsModal.updateOne(
+        { _id: myObjectId },
+        { departments: depsData }
+      );
+      departmentsModal.find({}).then((channels) => {
+        const newObjectId = new mongoose.Types.ObjectId();
+        // console.log("channels", channels);
+        if (channels.length) {
+          // console.log(channels[0]?.departments);
+          res.json({ responseData: channels[0].departments });
+        } else {
+          res.json({ responsMessage: "NoChanelFounded" });
+        }
+      });
+    } catch (error) {}
+  });
 });
 
 export default crearteNewDeprtment;
