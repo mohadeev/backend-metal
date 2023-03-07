fd2e9e3d1a16bcb4d2346db805f8d727cb03ef26
mohadeev <94206815+mohadeev@users.noreply.github.com>
2023-03-07 04:53:14 +0100

    F

--- routes/tour/get/tourData.js
+++ routes/tour/get/tourData.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import tourModal from "../../../db/schema/productModal.js";
 const tourData = express.Router();
-
+//fgfg
 tourData.get("/get/tour-data/:tourId", async (req, res) => {
   const tourId = req.params.tourId;
   if (mongoose.Types.ObjectId.isValid(tourId)) {
